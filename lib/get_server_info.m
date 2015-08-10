d281c40a2a37b3a14c38c494df571ba1803332da
sehagler <sehagler@gmail.com>
2015-08-10 14:33:54 -0700

    Revert "User can specify server"
    This reverts commit 81c7232b10abf40192c646904ff47df7b0f05c36.

--- lib/get_server_info.m
+++ lib/get_server_info.m
@@ -2,12 +2,11 @@ function server = get_server_info()
 %GET_SERVER_INFO prompt user for username and password
 %   GET_SERVER_INFO()
 
-% 08/10/15 - Initial revision - Stuart Hagler
-% 08/10/15 - User can specify server - Stuart Hagler
+% 11/13/12 - Initial revision - Stuart Hagler
 
-    server{1}.name = input('Enter server:  ', 's');
-    server{1}.username = input('Enter username:  ', 's');
-    server{1}.password = input('Enter password:  ', 's');
+    server{1}.name = 'mysql1.bme.ohsu.edu';
+    server{1}.username = input('Enter your mysql1 username:  ', 's');
+    server{1}.password = input('Enter your mysql1 password:  ', 's');
 
 end
 
